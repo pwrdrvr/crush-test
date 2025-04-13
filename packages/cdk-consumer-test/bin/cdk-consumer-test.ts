a8b4ad753af47bdb4eab8d393bd02ede17d36c3d
Harold Hunt <hhunt@giphy.com>
2025-04-13 12:32:40 -0400

    Fix test stack

--- packages/cdk-consumer-test/bin/cdk-consumer-test.ts
+++ packages/cdk-consumer-test/bin/cdk-consumer-test.ts
@@ -3,18 +3,31 @@ import * as cdk from 'aws-cdk-lib';
 import { CdkConsumerTestStack } from '../lib/cdk-consumer-test-stack';
 
 const app = new cdk.App();
-new CdkConsumerTestStack(app, 'CdkConsumerTestStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+// Main stack (always present)
+const mainStackName = 'crush-test-construct-test';
+const mainFunctionName = 'crush-test-construct-test-lambda';
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new CdkConsumerTestStack(app, mainStackName, {
+  stackName: mainStackName,
+  functionName: mainFunctionName,
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION || 'us-east-2',
+  },
 });
+
+// PR stack (only if PR_NUMBER is set)
+const prNumber = process.env.PR_NUMBER;
+if (prNumber) {
+  const prStackName = `crush-test-construct-test-pr-${prNumber}`;
+  const prFunctionName = `crush-test-construct-test-lambda-pr-${prNumber}`;
+  new CdkConsumerTestStack(app, prStackName, {
+    stackName: prStackName,
+    functionName: prFunctionName,
+    env: {
+      account: process.env.CDK_DEFAULT_ACCOUNT,
+      region: process.env.CDK_DEFAULT_REGION || 'us-east-2',
+    },
+  });
+}
--- packages/cdk-consumer-test/lib/cdk-consumer-test-stack.ts
+++ packages/cdk-consumer-test/lib/cdk-consumer-test-stack.ts
@@ -2,11 +2,20 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CrushTest } from '@pwrdrvr/crush-test-cdk';
 
+export interface CdkConsumerTestStackProps extends cdk.StackProps {
+  /**
+   * The name of the Lambda function. If not provided, a default will be used.
+   */
+  readonly functionName?: string;
+}
+
 export class CdkConsumerTestStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+  constructor(scope: Construct, id: string, props: CdkConsumerTestStackProps = {}) {
+    super(scope, props.stackName ?? id, props);
 
     // Instantiate the CrushTest construct from the packaged module
-    new CrushTest(this, 'TestCrushTest', {});
+    new CrushTest(this, 'TestCrushTest', {
+      functionName: props.functionName,
+    });
   }
 }
